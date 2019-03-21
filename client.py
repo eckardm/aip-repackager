e4d2297525d35c240692d4ea78ac365d3f4c47a5
Max Eckard <eckardm@umich.edu>
2019-03-21 13:20:46 -0400

    Zipped and unzipped AIPs, then repackaging into objects and metadata

--- client.py
+++ client.py
@@ -6,6 +6,8 @@ import shutil
 config = configparser.ConfigParser()
 config.read('config.ini')
 
+accession_id = 170864
+
 '''
 # unarchiving
 # use this when starting with 7z files
@@ -23,81 +25,19 @@ for root, _, files in os.walk(config['to-do']['path']):
         # subprocess.check_call(cmd)'''
 
 '''
+# use this when not starting with 7z files
+for root, dirs, files in os.walk(os.path.join(config['to-do']['path'], str(accession_id)), topdown=True):
+    depth = root[len(os.path.join(config['to-do']['path'], str(accession_id))) + len(os.path.sep):].count(os.path.sep)
+    if depth == 8:
+
+        print 'unarchiving ' + root
+
+        shutil.move(os.path.join(root), os.path.join(config['doing']['path']))'''
+
 # repackaging
 for name in os.listdir(config['doing']['path']):
 
     print 'repackaging ' + name
-
-    # objects
-    os.mkdir(os.path.join(config['doing']['path'], name, 'objects'))
-    for item in os.listdir(os.path.join(config['doing']['path'], name, 'data', 'objects')):
-
-        if item in ['metadata', 'submissionDocumentation']:
-            continue
-        os.rename(
-            os.path.join(config['doing']['path'], name, 'data', 'objects', item),
-            os.path.join(config['doing']['path'], name, 'objects', item)
-        )
-
-    os.chdir(os.path.join(config['doing']['path'], name))
-    cmd = [
-        os.path.join(config['p7zip_16.02']['path'], 'bin', '7za'), 'a',
-        '-bd',
-        '-tzip',
-        '-y',
-        '-mtc=on',
-        '-mmt=on',
-        os.path.join('objects.zip'),
-        os.path.join('objects')
-    ]
-    subprocess.check_call(cmd)
-
-    os.chdir(os.path.join('..', '..'))
-    shutil.rmtree(os.path.join(config['doing']['path'], name, 'objects'))
-
-    # metadata
-    os.chdir(config['doing']['path'])
-    cmd = [
-        os.path.join(config['p7zip_16.02']['path'], 'bin', '7za'), 'a',
-        '-bd',
-        '-tzip',
-        '-y',
-        '-mtc=on',
-        '-mmt=on',
-        '-x!' + os.path.join(name, 'objects.zip'),
-        os.path.join(name, 'metadata.zip'),
-        name
-    ]
-    subprocess.check_call(cmd)
-
-    os.chdir(os.path.join('..'))
-    for item in os.listdir(os.path.join(config['doing']['path'], name)):
-        if item in ['obects.zip', 'metadata.zip']:
-            continue
-
-        elif item == 'data':
-            shutil.rmtree(os.path.join(config['doing']['path'], name, item))
-
-        elif item in ['bag-info.txt', 'bagit.txt', 'manifest-sha256.txt', 'tagmanifest-md5.txt']:
-            os.remove(os.path.join(config['doing']['path'], name, item))'''
-
-
-# repackaging hashed
-
-
-# use this when not starting with 7z files
-'''
-for root, dirs, files in os.walk(config['to-do']['path'], topdown=True):
-    depth = root[len(config['to-do']['path']) + len(os.path.sep):].count(os.path.sep)
-    if depth == 8:
-
-        print 'repackaging ' + root
-
-        shutil.move(os.path.join(root), os.path.join(config['doing']['path']))'''
-
-for name in os.listdir(config['doing']['path']):
-
-    print name
 
     # objects
     os.mkdir(os.path.join(config['doing']['path'], name, 'objects'))
